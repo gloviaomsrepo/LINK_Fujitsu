7b3052b629638a1535375f98acb6411b287a3112
GLOVIA OMS <51308452+gloviaomsrepo@users.noreply.github.com>
2019-07-17 11:23:01 -0500

    Added config file
    Added config file

---
+++ test/integration/it.config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var getConfig = require('@tridnguyen/config');
+
+var opts = Object.assign({}, getConfig({
+    baseUrl: 'https://' + global.baseUrl,
+    suite: '*',
+    reporter: 'spec',
+    timeout: 60000,
+    locale: 'x_default'
+}, './config.json'));
+
+module.exports = opts;
