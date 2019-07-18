5bf825b37afe286dfb2dbe219b778095e8232e21
GLOVIA OMS <51308452+gloviaomsrepo@users.noreply.github.com>
2019-07-18 18:24:49 -0500

    Updated files after Linting
    Updated files after Linting

--- cartridges/int_gloviaom_sfra/cartridge/scripts/hooks/GloviaOm_Order.js
+++ cartridges/int_gloviaom_sfra/cartridge/scripts/hooks/GloviaOm_Order.js
@@ -1,10 +1,11 @@
-/******Import Library of Commerce Cloud******/
-importPackage( dw.system );
-importPackage( dw.order );
-
-/**************Before PATCH Trigger**********************/
-exports.beforePATCH  = function(order, orderInput)
-{
-	order.setExternalOrderNo(orderInput.c_externalOrderNo);
-	return new Status(Status.OK);
+/*
+Import Library of Commerce Cloud
+*/
+var Status = require('dw/system/Status');
+/*
+Before PATCH Trigger
+*/
+exports.beforePATCH = function (order, orderInput) {
+  order.setExternalOrderNo(orderInput.c_externalOrderNo);
+  return new Status(Status.OK);
 };
