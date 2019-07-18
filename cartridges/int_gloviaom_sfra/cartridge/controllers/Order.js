f6e9b1098d6156ffff68e229e490b4a051d85920
GLOVIA OMS <51308452+gloviaomsrepo@users.noreply.github.com>
2019-07-18 18:22:38 -0500

    Updated files after Linting
    Updated files after Linting

--- cartridges/int_gloviaom_sfra/cartridge/controllers/Order.js
+++ cartridges/int_gloviaom_sfra/cartridge/controllers/Order.js
@@ -1,11 +1,10 @@
 'use strict';
 var csrfProtection = require('*/cartridge/scripts/middleware/csrf');
 var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
-
-var server = require('server');
-server.extend(module.superModule);
 var GLOVIAOM = require('~/cartridge/scripts/GloviaOm');
 var LOGGER = require('dw/system/Logger');
+var server = require('server');
+server.extend(module.superModule);
 
 
 server.prepend('Confirm',
@@ -13,21 +12,22 @@ server.prepend('Confirm',
     consentTracking.consent,
     csrfProtection.generateToken,
     function (req, res, next) {
-        if (req.currentCustomer.raw.isAuthenticated()) {
-			var resData = GLOVIAOM.createOrderWithLines(req.querystring.ID);
-        }
-        next();
+      if (req.currentCustomer.raw.isAuthenticated()) {
+        LOGGER.debug('::Order Confirm::');
+        GLOVIAOM.createOrderWithLines(req.querystring.ID);
+      }
+      next();
     });
+
 server.prepend('Details',
-	    server.middleware.https,
-	    consentTracking.consent,
-	    csrfProtection.generateToken,
-	    function (req, res, next) {
-	        if (req.currentCustomer.raw.isAuthenticated()) {
-	                                                LOGGER.debug("::Order Details::");
-	                                                var resData = GLOVIAOM.createOrderWithLines(req.querystring.orderID);
-	                                                LOGGER.debug("::resData="+ resData);
-	        }
-	        next();
-	    });
+    server.middleware.https,
+    consentTracking.consent,
+    csrfProtection.generateToken,
+    function (req, res, next) {
+      if (req.currentCustomer.raw.isAuthenticated()) {
+        LOGGER.debug('::Order Details::');
+        GLOVIAOM.createOrderWithLines(req.querystring.orderID);
+      }
+      next();
+    });
 module.exports = server.exports();
