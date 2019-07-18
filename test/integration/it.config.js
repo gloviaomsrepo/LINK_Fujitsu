ce9ed1b04c421d66ba83db770b143b429b9618ab
GLOVIA OMS <51308452+gloviaomsrepo@users.noreply.github.com>
2019-07-18 18:18:16 -0500

    Updated files after Linting
    Updated files after Linting

--- test/integration/it.config.js
+++ test/integration/it.config.js
@@ -3,11 +3,11 @@
 var getConfig = require('@tridnguyen/config');
 
 var opts = Object.assign({}, getConfig({
-    baseUrl: 'https://' + global.baseUrl,
-    suite: '*',
-    reporter: 'spec',
-    timeout: 60000,
-    locale: 'x_default'
+  baseUrl: 'https://' + global.baseUrl,
+  suite: '*',
+  reporter: 'spec',
+  timeout: 60000,
+  locale: 'x_default'
 }, './config.json'));
 
 module.exports = opts;
