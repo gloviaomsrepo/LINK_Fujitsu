653ff2ed141f3c2cbab164f123062932bab6ba31
GLOVIA OMS <51308452+gloviaomsrepo@users.noreply.github.com>
2019-07-18 18:17:37 -0500

    Updated files after Linting
    Updated files after Linting

--- test/util.js
+++ test/util.js
@@ -2,29 +2,29 @@
 var ArrayList = require('./mocks/dw.util.Collection');
 
 module.exports = function toProductMock(mock) {
-    if (!mock || typeof mock === 'function' || mock instanceof ArrayList) {
-        return mock;
-    }
+  var result = {};
+  var innerMock;
+  if (!mock || typeof mock === 'function' || mock instanceof ArrayList) {
+    return mock;
+  }
+  if (typeof mock === 'object') {
+    Object.keys(mock).forEach(function (item) {
+      if (typeof mock[item] === 'object') {
+        if (mock[item] && mock[item].type === 'function') {
+          innerMock = typeof mock[item].return !== 'undefined'
+            ? toProductMock(mock[item].return)
+            : toProductMock(mock[item]);
+          result[item] = function () { return innerMock; };
+        } else {
+          result[item] = toProductMock(mock[item]);
+        }
+      } else if (item !== 'function' || item !== 'return') {
+        result[item] = mock[item];
+      }
+    });
+  } else {
+    result = mock;
+  }
 
-    var result = {};
-    if (typeof mock === 'object') {
-        Object.keys(mock).forEach(function (item) {
-            if (typeof mock[item] === 'object') {
-                if (mock[item] && mock[item].type === 'function') {
-                    var innerMock = typeof mock[item].return !== 'undefined'
-                        ? toProductMock(mock[item].return)
-                        : toProductMock(mock[item]);
-                    result[item] = function () { return innerMock; };
-                } else {
-                    result[item] = toProductMock(mock[item]);
-                }
-            } else if (item !== 'function' || item !== 'return') {
-                result[item] = mock[item];
-            }
-        });
-    } else {
-        result = mock;
-    }
-
-    return result;
+  return result;
 };
