44bfcdefceebe14d696379415b158753d22cf657
GLOVIA OMS <51308452+gloviaomsrepo@users.noreply.github.com>
2020-06-30 22:40:15 -0500

    Updating scripts for Cartridge Re-Certification

--- cartridges/int_gloviaom_sfra/cartridge/scripts/util/GloviaOm_Token.js
+++ cartridges/int_gloviaom_sfra/cartridge/scripts/util/GloviaOm_Token.js
@@ -1,23 +1,15 @@
 'use strict';
 /* Script Modules */
 var Site = require('dw/system/Site');
-/*
-var params = request.httpParameterMap;
-var OAuthLoginFlowMgr = require('dw/customer/oauth/OAuthLoginFlowMgr');
-var Transaction = require('dw/system/Transaction');
-var URLUtils = require('dw/web/URLUtils');
-*/
-
 var LOGGER = require('dw/system/Logger');
 var Status = require('dw/system/Status');
 var ServiceFactory = require('~/cartridge/scripts/util/ServiceFactory');
 var GloviaOmFactory = require('~/cartridge/scripts/util/GloviaOmFactory');
 
 function getToken() {
-  var returnStatus;
-  var sitePref = 'GLOVIAOM_ServiceName';
-  var sitePrefGLOVIA = Site.getCurrent().getPreferences();
-  var serviceFrameworkName = sitePrefGLOVIA.getCustom()[sitePref];
+  var sitePref = Site.getCurrent().getPreferences();
+  var serviceFrameworkName = sitePref.getCustom().GLOVIAOM_ServiceName;
+  // create service object using local registery
   var service = ServiceFactory.getServiceRegistry({
     serviceName: serviceFrameworkName,
     feature: GloviaOmFactory.FEATURES.TOKEN
@@ -25,25 +17,35 @@ function getToken() {
   var ServiceConfigVar = service.getConfiguration();
   var ServiceCredentialVar = ServiceConfigVar.getCredential();
   var ExtensibleObject = ServiceCredentialVar.getCustom();
-  var requestDataContainer = 'grant_type=password'
-    + '&client_id=' + ExtensibleObject.ClientId
-	+ '&client_secret=' + ExtensibleObject.ClientSecret
-	+ '&username=' + ServiceCredentialVar.getUser()
-	+ '&password=' + ServiceCredentialVar.getPassword();
+  var requestDataContainer = 'grant_type=password&client_id='
+  + ExtensibleObject.ClientId + '&client_secret=' + ExtensibleObject.ClientSecret
+  + '&username=' + ServiceCredentialVar.getUser() + '&password='
+  + ServiceCredentialVar.getPassword();
+  // make the call
   var result = service.call(requestDataContainer);
   var responseObj;
-  if (result.error !== 0 || result.errorMessage != null || result.mockResult) {
+  var returnStatus;
+  // error case
+  if (result.error !== 0 || result.errorMessage !== null || result.mockResult) {
+    // setting response object in status
     responseObj = JSON.parse(result.object);
     returnStatus = new Status(Status.ERROR, GloviaOmFactory.STATUS_CODES.GENERAL.SUCCESS_ERROR);
     if (result && 'object' in result) {
       returnStatus.addDetail(GloviaOmFactory.STATUS_CODES.RESPONSE_OBJECT, result.object);
     }
+
+    LOGGER.debug('::::error-data=' + JSON.stringify(responseObj));
+    LOGGER.debug('::::error-result.error=' + result.error);
+    LOGGER.debug('::::error-result.errorMessage=' + result.errorMessage);
+    LOGGER.debug('::::error-result.mockResult=' + result.mockResult);
   } else {
+    // setting response object in status
     responseObj = JSON.parse(result.object);
-    LOGGER.debug('::responseObj=' + responseObj);
+    LOGGER.debug('::::token-data=' + JSON.stringify(responseObj));
   }
   return responseObj;
 }
+// exports.GetToken.public = true;
 module.exports = {
   getToken: getToken
 };
