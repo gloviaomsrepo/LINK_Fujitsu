e863463149809065ff0351420808fa4401ca9840
GLOVIA OMS <51308452+gloviaomsrepo@users.noreply.github.com>
2020-06-30 22:38:33 -0500

    Updating scripts for Cartridge Re-Certification

--- cartridges/int_gloviaom_sfra/cartridge/scripts/GloviaOm.js
+++ cartridges/int_gloviaom_sfra/cartridge/scripts/GloviaOm.js
@@ -9,6 +9,9 @@ var GloviaOmFactory = require('~/cartridge/scripts/util/GloviaOmFactory');
 var Site = require('dw/system/Site');
 
 function createOrderWithLines(orderNo) {
+  var PAYMENT_RECORDTYPEID = '012f4000000JhYo';
+  var SHIPPING_RECORDTYPEID = '012f4000000JhYj';
+  var PRODUCTLINE_RECORDTYPEID = '012f4000000JhYe';
   var orderInfo = OrderMgr.getOrder(orderNo);
   var customerNo = orderInfo.customer.profile.customerNo;
   var customerDetailJSON = '';
@@ -16,44 +19,39 @@ function createOrderWithLines(orderNo) {
   var billingAddressJSON = '';
   var ShipAddressFromShipments = orderInfo.getShipments();
   var shippingAddressJSON = '';
+  var shippingAddress;
+  var shippingMethod;
+  var PaymentInstruments = orderInfo.getPaymentInstruments();
+  var paymentJSON = '';
+  var PaymentInstrument;
+  var PaymentTransaction;
   var Shipments = orderInfo.getShipments();
   var shipmentsJSON = '';
+  var shipment;
+  var i = 0;
   var orderLines = orderInfo.getProductLineItems();
   var productLinesJSON = '';
-  var i = 0;
-  var shippingAddress = '';
-  var shippingMethod = '';
-  var shipment;
   var totalPrice;
   var productLineItem;
-  var shipmentAndLines = '';
-  var body = '';
+  var shipmentAndLines;
+  var body;
   var resData = '';
   var org = TOKEN.getToken();
   var sitePref = Site.getCurrent().getPreferences();
   var serviceFrameworkName = sitePref.getCustom().GLOVIAOM_ServiceName;
-  var service = ServiceFactory.getServiceRegistry({
-    serviceName: serviceFrameworkName,
-    feature: GloviaOmFactory.FEATURES.ORDER_CREATE
-  });
+  var service;
   var result;
   var responseObj;
   var returnStatus;
 
-  /** *********Customer Detail -START***********/
   customerDetailJSON += '"giic_Account__c" : "' + customerNo + '",';
   customerDetailJSON += '"giic_Source__c" : "Online Sale",';
-  /** *********Customer Detail -END***********/
 
-  /** *********Billing Addres -Start***********/
   billingAddressJSON += '"giic_BillingStreet__c" : "' + billingAddress.address1 + '",';
   billingAddressJSON += '"giic_BillingCity__c" : "' + billingAddress.city + '",';
   billingAddressJSON += '"giic_BillingStateProvince__c" : "' + billingAddress.stateCode + '",';
   billingAddressJSON += '"giic_BillingCountry__c" : "' + billingAddress.countryCode + '",';
   billingAddressJSON += '"giic_BillingZipPostalCode__c" : "' + billingAddress.postalCode + '",';
-  /** *********Billing Addres -END***********/
-
-  /** *********Shipping Addres -Start***********/
 
   for (i = 0; i < ShipAddressFromShipments.size(); i++) {
     shippingAddress = ShipAddressFromShipments[i].getShippingAddress();
@@ -62,32 +60,66 @@ function createOrderWithLines(orderNo) {
     shippingAddressJSON += '"giic_ShipToStateProvince__c" : "' + shippingAddress.stateCode + '",';
     shippingAddressJSON += '"giic_ShipToCountry__c" : "' + shippingAddress.countryCode + '",';
     shippingAddressJSON += '"giic_ShipToZipPostalCode__c" : "' + shippingAddress.postalCode + '",';
+
+    shippingMethod = ShipAddressFromShipments[i].getShippingMethodID();
+    if (shippingMethod !== null && shippingMethod === '005') {
+      customerDetailJSON += '"giic_WarehouseCode__c" : "'
+      + ShipAddressFromShipments[i].custom.fromStoreId + '",';
+    }
     break;
   }
-  /** *********Billing Addres -END***********/
 
-  /** **********Shipping Charge -START***************/
+  if (PaymentInstruments.size() > 0) {
+    paymentJSON = '';
+    for (i = 0; i < PaymentInstruments.size(); i++) {
+      PaymentInstrument = PaymentInstruments[i];
+      PaymentTransaction = PaymentInstrument.getPaymentTransaction();
+      paymentJSON += '{' +
+      '       "attributes": {' +
+      '       "type": "gii__SalesOrderLineStaging__c",' +
+      '        "referenceId": "' + PaymentTransaction.getTransactionID() + '-PAYMENT"' +
+      '     },';
+      paymentJSON += ' "giic_PaymentMethod__c": "' + PaymentInstrument.getPaymentMethod() + '"';
+      paymentJSON += ',"giic_CreditCardType__c":"' + PaymentInstrument.getCreditCardType() + '"';
+      paymentJSON += ',"giic_CreditCardNumberLastDigits__c":"'
+      + PaymentInstrument.getCreditCardNumberLastDigits() + '"';
+      paymentJSON += ',"giic_CreditCardExpirationMonth__c":"'
+      + PaymentInstrument.getCreditCardExpirationMonth() + '"';
+      paymentJSON += ',"giic_CreditCardExpirationYear__c":"'
+      + PaymentInstrument.getCreditCardExpirationYear() + '"';
+      paymentJSON += ',"giic_CreditCardHolder__c":"'
+      + PaymentInstrument.getCreditCardHolder() + '"';
+      paymentJSON += ',"giic_PaymentAmount__c":"'
+      + PaymentTransaction.getAmount() + '"';
+      paymentJSON += ',"giic_PaymentTransactionId__c":"'
+      + PaymentTransaction.getTransactionID() + '"';
+      paymentJSON += ',"RecordTypeId":"' + PAYMENT_RECORDTYPEID + '"';
+      paymentJSON += '},';
+    }
+  }
+
   if (Shipments.size() > 0) {
-    shipmentsJSON = '"records": [';
-    i = 0;
+    shipmentsJSON = '';
     for (i = 0; i < Shipments.size(); i++) {
       shipment = Shipments[i];
       shippingMethod = shipment.getShippingMethod();
       shipmentsJSON += '{' +
       '       "attributes": {' +
       '       "type": "gii__SalesOrderLineStaging__c",' +
-      '        "referenceId": "' + shippingMethod.getID() + '"' +
+      '        "referenceId": "' + shipment.getShipmentNo() + '"' +
       '     },';
       shipmentsJSON += ' "giic_ShippingId__c": "' + shippingMethod.getID() + '"';
-      shipmentsJSON += ',"giic_ShippingDescription__c":"' + shippingMethod.getDescription() + '"';
-      shipmentsJSON += ',"giic_ShippingUnitPrice__c":"' + shipment.getShippingTotalPrice() + '"';
-      shipmentsJSON += ',"giic_Shipping_Tax__c":"' + shipment.getShippingTotalTax() + '"';
+      shipmentsJSON += ',"giic_ShippingNumber__c": "' + shipment.getShipmentNo() + '"';
+      shipmentsJSON += ',"giic_ShippingDescription__c":"'
+      + shippingMethod.getDescription() + '"';
+      shipmentsJSON += ',"giic_ShippingUnitPrice__c":"'
+      + shipment.getShippingTotalPrice() + '"';
+      shipmentsJSON += ',"giic_ShippingTax__c":"' + shipment.getShippingTotalTax() + '"';
+      shipmentsJSON += ',"RecordTypeId":"' + SHIPPING_RECORDTYPEID + '"';
       shipmentsJSON += '},';
     }
   }
-  /** **********Shipping Charge -END***************/
 
-  /** *********Order Lines -START***********/
   if (orderLines.size() > 0) {
     for (i = 0; i < orderLines.size(); i++) {
       if (i > 0) {
@@ -100,42 +132,45 @@ function createOrderWithLines(orderNo) {
       + '       "type": "gii__SalesOrderLineStaging__c",'
       + '        "referenceId": "' + orderLines[i].orderItem.itemID + '"'
       + '     },';
-      productLinesJSON += '"giic_ProductSKU__c": "'
-      + productLineItem.getProductID()
-      + '","giic_OrderQuantity__c":"'
-      + orderLines[i].quantity
-      + '","giic_UnitPrice__c":"'
-      + orderLines[i].price + '"';
+      productLinesJSON += '"giic_ProductSKU__c": "' + productLineItem.getProductID()
+      + '","giic_OrderQuantity__c":"' + orderLines[i].quantity
+      + '","giic_UnitPrice__c":"' + orderLines[i].price + '"';
       productLinesJSON += ',"giic_LineNo__c":"' + orderLines[i].orderItem.itemID + '"';
       productLinesJSON += ',"giic_TaxAmount__c":"' + orderLines[i].tax + '"';
-      productLinesJSON += ',"giic_LineDescription__c":"' + orderLines[i].productName + '"';
+      productLinesJSON += ',"giic_LineDescription__c":"'
+      + (orderLines[i].productName).replace('"', '\\"') + '"';
       productLinesJSON += ',"giic_ProductAmount__c":"' + totalPrice + '"';
-      LOGGER.debug('::UPC::' + orderLines[i].product.UPC);
-      LOGGER.debug('::ProductId::' + productLineItem.getProductID());
+      productLinesJSON += ',"RecordTypeId":"' + PRODUCTLINE_RECORDTYPEID + '"';
+      LOGGER.debug('::UPC=' + orderLines[i].product.UPC);
+      LOGGER.debug('::ProductID=' + productLineItem.getProductID());
       productLinesJSON += '}';
     }
   }
-  shipmentAndLines = shipmentsJSON + productLinesJSON + ']';
-  /** *********Order Lines -END***********/
+
+  shipmentAndLines = '"records": [' + paymentJSON + shipmentsJSON + productLinesJSON + ']';
   body = '{'
-    + '"records": ['
-    + '{'
-    + '"attributes": {'
-    + '"type": "gii__SalesOrderStaging__c",'
-    + '"referenceId": "' + orderNo + '" '
-    + '},'
-    + customerDetailJSON
-    + billingAddressJSON
-    + shippingAddressJSON
-    + '	"giic_OrderNumber__c":"' + orderNo + '",'
-    + ' "SalesOrderLineStagings__r": {'
-    + shipmentAndLines
-    + ' }} ]}';
+  + '"records": ['
+  + '{'
+  + '"attributes": {'
+  + '"type": "gii__SalesOrderStaging__c",'
+  + '"referenceId": "' + orderNo + '" '
+  + '},'
+  + customerDetailJSON
+  + billingAddressJSON
+  + shippingAddressJSON
+  + '	"giic_OrderNumber__c":"' + orderNo + '",'
+  + ' "SalesOrderLineStagings__r": {'
+  +	shipmentAndLines
+  + ' }} ]}';
+
+  service = ServiceFactory.getServiceRegistry({
+    serviceName: serviceFrameworkName,
+    feature: GloviaOmFactory.FEATURES.ORDER_CREATE
+  });
 
   service.addHeader('Authorization', org.token_type + ' ' + org.access_token);
-  service.setURL(org.instance_url +
-  GloviaOmFactory.CONFIGURATIONS.ORDER_CREATE.ORDER_STAGING_SUFFIX);
-
+  service.setURL(org.instance_url
+  + GloviaOmFactory.CONFIGURATIONS.ORDER_CREATE.ORDER_STAGING_SUFFIX);
   // make the call
   result = service.call(body);
   // error case
@@ -146,13 +181,15 @@ function createOrderWithLines(orderNo) {
     if (result && 'object' in result) {
       returnStatus.addDetail(GloviaOmFactory.STATUS_CODES.RESPONSE_OBJECT, result.object);
     }
-    LOGGER.debug('::::gloviaom-staging-error=' + result.errorMessage);
+    LOGGER.debug('::::error-data=' + JSON.stringify(responseObj));
+    LOGGER.debug('::::error-result.error=' + result.error);
+    LOGGER.debug('::::error-result.errorMessage=' + result.errorMessage);
+    LOGGER.debug('::::error-result.mockResult=' + result.mockResult);
   } else {
     // setting response object in status
     responseObj = JSON.parse(result.object);
-    LOGGER.debug('::::gloviaom-staging-data=' + JSON.stringify(responseObj));
+    LOGGER.debug('::::staging-data=' + JSON.stringify(responseObj));
   }
-
   return resData;
 }
 
