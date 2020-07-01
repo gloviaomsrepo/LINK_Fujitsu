d2f0c5ab738cda2e9ccf587cc36584a16480d6a1
GLOVIA OMS <51308452+gloviaomsrepo@users.noreply.github.com>
2020-06-30 22:39:25 -0500

    Updating hook for Cartridge Re-Certification

--- cartridges/int_gloviaom_sfra/cartridge/scripts/hooks/GloviaOm_Order.js
+++ cartridges/int_gloviaom_sfra/cartridge/scripts/hooks/GloviaOm_Order.js
@@ -7,5 +7,9 @@ Before PATCH Trigger
 */
 exports.beforePATCH = function (order, orderInput) {
   order.setExternalOrderNo(orderInput.c_externalOrderNo);
+  if (orderInput.c_shippedFlag !== null && orderInput.c_shippedFlag !== 0) {
+    order.setShippingStatus(orderInput.c_shippedFlag);
+    order.setStatus(5);
+  }
   return new Status(Status.OK);
 };
